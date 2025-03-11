a9aa9eddf3d8fb94659e95bc0b079bfda583f6f1
Noel Welsh <noel@noelwelsh.com>
2025-03-11 17:21:31 +0000

    Reformat

--- src/main/scala/aui/Component.scala
+++ src/main/scala/aui/Component.scala
@@ -7,17 +7,27 @@ extension [Alg <: Algebra, A](component: Component[Alg, A]) {
   def map[B](f: A => B): Component[Alg & Map, B] =
     alg => alg.map(component.apply(alg), f)
 
-  def and[Alg2 <: Algebra, B](that: Component[Alg2, B]): Component[Alg & Alg2 & Layout, (A, B)] =
+  def and[Alg2 <: Algebra, B](
+      that: Component[Alg2, B]
+  ): Component[Alg & Alg2 & Layout, (A, B)] =
     alg => alg.and(component.apply(alg), that.apply(alg))
 }
+
 /** Constructors for atomic UI elements. */
 object Component {
   def text(content: String): Component[Text, Unit] =
     alg => alg.text(content)
 
-  def textField(label: String, placeholder: String = "", validation: Validation[String] = Validation.succeed): Component[Controls, String] =
+  def textField(
+      label: String,
+      placeholder: String = "",
+      validation: Validation[String] = Validation.succeed
+  ): Component[Controls, String] =
     alg => alg.textField(label, placeholder, validation)
 
-  def choice[A](label: String, options: Seq[(String, A)]): Component[Controls, A] =
+  def choice[A](
+      label: String,
+      options: Seq[(String, A)]
+  ): Component[Controls, A] =
     alg => alg.choice(label, options)
 }
--- src/main/scala/aui/algebra/Controls.scala
+++ src/main/scala/aui/algebra/Controls.scala
@@ -1,7 +1,11 @@
 package aui.algebra
 
 trait Controls extends Algebra {
-  def textField(label: String, placeholder: String, validation: Validation[String]): Ui[String]
+  def textField(
+      label: String,
+      placeholder: String,
+      validation: Validation[String]
+  ): Ui[String]
 
   def choice[A](label: String, options: Seq[(String, A)]): Ui[A]
 }
